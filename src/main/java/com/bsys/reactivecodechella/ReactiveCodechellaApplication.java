0559b61f8f0b2970083a560f6dfc7d5195d11def
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-06-29 07:58:30 -0300

    initi al commit

---
+++ src/main/java/com/bsys/reactivecodechella/ReactiveCodechellaApplication.java
@@ -0,0 +1,13 @@
+package com.bsys.reactivecodechella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCodechellaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCodechellaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsys/reactivecodechella/ReactiveCodechellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsys.reactivecodechella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCodechellaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
